081d556a1a41389af3e0f21024f5ce84575dadff
Antreo Pukay <acrosswalker@yahoo.com>
2018-10-15 16:10:51 -0700

    app.vue created

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Pateh Parsons</h1>
+
+    <nav>
+      <router-link to="/">Home</router-link>
+      &nbsp;
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
+
+<style>
+
+h1 {
+  text-align: center;
+}
+
+nav {
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueRouter from 'Vue-router';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router';
 
