182137f254bc610ce2a53daeb30768a136a52b15
Antreo Pukay <acrosswalker@yahoo.com>
2018-10-16 16:14:48 -0700

    adding background image

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <h1>Pateh Parsons</h1>
-
+    <header>
+      <h1>Pateh Parsons</h1>
+    </header>
+      <hr/>
     <nav>
-      <router-link to="/">Home</router-link>
-      &nbsp;
-      <router-link to="/art">Art</router-link>
-      &nbsp;
-      <router-link to="/music">Music</router-link>
-      &nbsp;
-      <router-link to="/literature">Literature</router-link>
-      &nbsp;
-      <router-link to="/about">About</router-link>
-      &nbsp;
+      <section id="container">
+          <router-link to="/">Home</router-link>
+          &nbsp;
+          <router-link to="/art">Art</router-link>
+          &nbsp;
+          <router-link to="/music">Music</router-link>
+          &nbsp;
+          <router-link to="/literature">Literature</router-link>
+          &nbsp;
+          <router-link to="/about">About</router-link>
+          &nbsp;
+      </section>
+      <hr/>
     </nav>
+
     <router-view></router-view>
   </div>
 </template>
@@ -31,11 +37,26 @@ body {
   background-color: lightblue;
 }
 
+body {
+  padding-left: 0;
+  background-image: url(./assets/psychadelic-ocean.jpg);
+  background-size: cover;
+  width: 100%;
+  height: auto;
+  padding-bottom: 30%;
+}
+
 h1 {
   text-align: center;
 }
 
-nav {
-  text-align: center;
+#container {
+  margin-left: 130px;
+  margin-right: 130px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
+
+
 </style>
