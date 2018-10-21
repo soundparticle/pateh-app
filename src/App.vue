3c3ef58991c3d42e77a21901ae2246e6d37adab8
Antreo Pukay <acrosswalker@yahoo.com>
2018-10-20 22:15:10 -0700

    minor change

--- src/App.vue
+++ src/App.vue
@@ -83,12 +83,12 @@ hr {
 }
 
 .hidden {
-  position:absolute;
-  left:-10000px;
-  top:auto;
-  width:1px;
-  height:1px;
-  overflow:hidden;
+  position: absolute;
+  left: -10000px;
+  top: auto;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
 }
 
 #container {
